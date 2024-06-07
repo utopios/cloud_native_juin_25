8e5fb1735fcae526b151a7d1069cb7b1c9d36751
ihab abadi <ihab@utopios.net>
2024-06-07 14:17:10 +0200

    debut du script sh

---
+++ etude_de_cas/script.sh
@@ -0,0 +1,13 @@
+### Phase 0 
+    ## 1. Modification des applications pour une utilisation de la configuration par variables d'environnement
+    ## 2. Fournir la configuration aux modules de déploiements (quarkus kubernetes)
+### Phase 1 Construction des images en natives avec Docker.
+
+### Phase 2 Modification des ressources kubernetes générées par le module (quarkus kubernetes)
+
+### Phase 3 vérification de la disponibilité d'un cluster kubernetes (localement kind)
+
+### Phase 4 Déployer l'application sur notre cluster
+
+### Phase 5 utilisation des ports du host pour tester notre application
+
